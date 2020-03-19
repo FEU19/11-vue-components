e8314103599c8c5a5ad7e807bbdf8fcbb1295f3b
David Andersson <lejonmanen@gmail.com>
2020-03-19 09:53:49 +0100

    Example with computed property

---
+++ src/components/Content.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="component">
+        <div>
+            {{ countMessage }}
+        </div>
+        <div>
+            <button @click="count += 1">Increase the count</button>
+            <button @click="increaseCount">Increase the count</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    props: {
+    },
+    data: () => ({
+        count: 1
+    }),
+    computed: {
+        /*function*/ countMessage() {
+            return `The count is ${this.count}.`;
+        }
+    },
+    methods: {
+        increaseCount() {
+            this.count += 2;
+        }
+    }
+}
+</script>
+
+<style scoped>
+.component {
+
+}
+</style>
